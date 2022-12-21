f8b43c6ec7740830a9cf96896c648a0788ab094e
BytsaiKonstantin <91729673+kbytsai@users.noreply.github.com>
2022-12-21 22:05:58 +0300

    Work on project

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
 import * as Functions from "./modules/functions.js";
 
+const d = document;
+const navigationButton = d.querySelector(".burger");
 Functions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+function toogleNavigation() {
+  const navigation = document.querySelector(".header__navigation");
+  navigation.classList.toggle("header__navigation_active");
+  navigationButton.classList.toggle("burger_active");
+}
+
+navigationButton.addEventListener("click", toogleNavigation, false);
